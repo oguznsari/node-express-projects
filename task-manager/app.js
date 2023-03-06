40d4b9d3d050e283593cbd8ddedf2906ccdefb73
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-06 23:27:50 +0300

    route&controller backbone structure

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,1 +1,19 @@
 console.log('Task Manager App')
+
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+const PORT = 3000;
+
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
---
+++ task-manager/controllers/tasks.js
@@ -0,0 +1,28 @@
+const getAllTasks = (req, res) => {
+    res.send('All items from the file.')
+}
+
+const createTask = (req, res) => {
+    res.send('Create Task')
+}
+
+const getTask = (req, res) => {
+    res.send('Get Single Task')
+}
+
+const updateTask = (req, res) => {
+    res.send('Update Task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Delete Task')
+}
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}

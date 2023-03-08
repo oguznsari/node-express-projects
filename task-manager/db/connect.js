9e18fe2734cde4b9a5bcc1d3ae793e14fb5f11af
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-08 18:08:00 +0300

    mongoose connection options

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const connectionString = "mongodb+srv://dbUser:0fNHUN2XG9ouxId2@freecc.d7ccarv.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&useUnifiedTopology=true";
 
 mongoose
-    .connect(connectionString)
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to the DB..."))
     .catch((err) => console.log({ err }))

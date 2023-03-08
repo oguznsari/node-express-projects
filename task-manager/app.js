f1ec1d881349feff3f178333ece935b04298d0d6
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-08 18:26:31 +0300

    dotenv to keep secret keys safe :) too late :(

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,9 +1,10 @@
 console.log('Task Manager App')
-require('./db/connect')
 
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require("dotenv").config()
 
 // middleware
 app.use(express.json())
@@ -17,4 +18,13 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
-app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`Server is listening on port ${PORT}...`))
+    } catch (error) {
+        console.log({ error })
+    }
+}
+
+start()
--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://dbUser:0fNHUN2XG9ouxId2@freecc.d7ccarv.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&useUnifiedTopology=true";
+const connectDB = (url) => {
+    return mongoose
+        .connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+}
 
-mongoose
-    .connect(connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to the DB..."))
-    .catch((err) => console.log({ err }))
+module.exports = connectDB

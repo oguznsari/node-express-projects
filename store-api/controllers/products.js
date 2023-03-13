639bc54060164539016c4fc41bc4b91b5c78b5c7
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-13 21:26:23 +0300

    generate queryObject to escape from non-existing params

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -7,7 +7,22 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     // console.log(req.query);
-    const products = await Product.find(req.query);
+    const { featured, company, name } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+
+    const products = await Product.find(queryObject);
     res.status(200).json({ products, nbHits: products.length });
 }
 

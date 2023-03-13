a9ef029a69c7c672fdb4aaf2f30a344c9cf5ec8d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-13 09:25:18 +0300

    Creating custom error message handler

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require("../models/Task");
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({});
@@ -11,17 +12,17 @@ const createTask = asyncWrapper(async (req, res) => {
     res.status(201).json({ task })
 })
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
     const { id: taskId } = req.params;
     const task = await Task.findOne({ _id: taskId })
 
     if (!task) {
-        res.status(404).json({ msg: `No task found with id: ${taskId}!` })
+        return next(createCustomError(`No task found with id: ${taskId}!`, 404))
     }
     res.status(201).json({ task })
 })
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
     const { id: taskId } = req.params;
     const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
         new: true,
@@ -29,22 +30,21 @@ const updateTask = asyncWrapper(async (req, res) => {
     });
 
     if (!task) {
-        return res.status(404).json({ msg: `No task found with id: ${taskId}` })
+        return next(createCustomError(`No task found with id: ${taskId}!`, 404))
     }
     res.status(200).json({ task })
 })
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
     const { id: taskId } = req.params;
     const task = await Task.findOneAndDelete({ _id: taskId })
 
     if (!task) {
-        res.status(404).json({ msg: `No task found with id: ${taskId}!` })
+        return next(createCustomError(`No task found with id: ${taskId}!`, 404))
     }
 
     res.status(200).json({ task })
 })
-
 
 module.exports = {
     getAllTasks,

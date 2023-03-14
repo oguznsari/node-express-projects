1447c59d51ae6d5e754f37774454cd52a743a335
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-14 10:23:18 +0300

    dashboard login/register - routes controller starting steps

---
+++ jwt-basics/controllers/main.js
@@ -0,0 +1,31 @@
+/*
+    Check username, password in the post(login) request
+    if exists create a new JWT
+    send back to front-end
+
+    Setup authentication so only the request with JWT can access the dashboard
+*/
+
+const CustomAPIError = require('../errors/custom-error');
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    console.log(username, password)
+    // mongoose validation 
+    // Joi
+    // check in the controller
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email and password.', 400)
+    }
+    res.send('Fake Login/Register/Signup Route');
+}
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        msg: `Hello John Doe`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}.`
+    });
+}
+
+module.exports = { login, dashboard }

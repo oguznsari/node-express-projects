57da07fd902a680c26f412d31f81ae51d82344de
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-15 10:46:24 +0300

    optional validation on controller

--- jobs-api/controllers/auth.js
+++ jobs-api/controllers/auth.js
@@ -1,7 +1,18 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+// const { BadRequestError } = require('../errors')
 
 const register = async (req, res) => {
-    res.send('Register User.')
+    /* optional validation on the controller (code repetition since mongoose already does that for us)
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        throw new BadRequestError('Please provide name, email and password.', StatusCodes.BAD_REQUEST)
+    }
+    */
+
+    const user = await User.create({ ...req.body })
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {

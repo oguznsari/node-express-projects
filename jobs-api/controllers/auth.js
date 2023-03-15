b74ec7f441f7a5f94c4066e7d9e1df855141cde2
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-15 17:25:26 +0300

    token generation in mongoose model (with custom middleware methods option)

--- jobs-api/controllers/auth.js
+++ jobs-api/controllers/auth.js
@@ -1,15 +1,13 @@
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 // const { BadRequestError } = require('../errors')
 
 const register = async (req, res) => {
-    const user = await User.create({ ...req.body })
-    const token = jwt.sign(
-        { userId: user._id, name: user.name },
-        process.env.JWT_SECRET,
-        { expiresIn: '30d' })
-    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
+    res
+        .status(StatusCodes.CREATED)
+        .json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {

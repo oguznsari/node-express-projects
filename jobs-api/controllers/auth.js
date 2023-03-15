15c83904c1350476fa42d471c989ed5f640fd4c4
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-15 10:19:52 +0300

    dbConn, routes, dummyControllers etc.

---
+++ jobs-api/controllers/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+    res.send('Register User.')
+}
+
+const login = async (req, res) => {
+    res.send('Login User.')
+}
+
+module.exports = {
+    register,
+    login
+}

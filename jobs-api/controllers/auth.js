56bed3d171ce615cf54a8b85ecc23e06da664f1d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-15 14:16:59 +0300

    bcrypt in the db model and added token logic

--- jobs-api/controllers/auth.js
+++ jobs-api/controllers/auth.js
@@ -2,20 +2,14 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 // const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    /* optional validation on the controller (code repetition since mongoose already does that for us)
-    if (!name || !email || !password) {
-        throw new BadRequestError('Please provide name, email and password.', StatusCodes.BAD_REQUEST)
-    }
-    */
-    const tempUser = { name, email, password: hashedPassword }
-    const user = await User.create({ ...tempUser })
-    res.status(StatusCodes.CREATED).json({ user })
+    const user = await User.create({ ...req.body })
+    const token = jwt.sign(
+        { userId: user._id, name: user.name },
+        process.env.JWT_SECRET,
+        { expiresIn: '30d' })
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {

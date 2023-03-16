3ef5d7c72589ede654e9e4cd1b71078d29b5cdde
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-16 17:09:41 +0300

    API documentation with SwaggerUI-Apimatic and Postman

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -7,6 +7,13 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
 const express = require('express');
 const app = express();
 
@@ -36,6 +43,11 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs Api</h1><a href="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);

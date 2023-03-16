996487838cb3469c14517b60fe4167bba40d6822
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-16 11:33:34 +0300

    deleteJob controller

--- jobs-api/controllers/jobs.js
+++ jobs-api/controllers/jobs.js
@@ -52,7 +52,18 @@ const updateJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
-    res.send('Delete Job.');
+    const {
+        user: { userId },
+        params: { id: jobId }
+    } = req;
+
+    const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id: ${jobId}. `)
+    }
+
+    res.status(StatusCodes.OK).send();
 }
 
 module.exports = {

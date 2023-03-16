8291e052a9f5df91e445cfbbe59d3b96b67ee6a9
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-16 16:18:02 +0300

    Security packages added (middlewares)

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -18,7 +25,17 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
+// extra security packages
+app.set('trust proxy', 1);      // we will host it in heroku and it uses reverse proxy 
+app.use(
+  rateLimiter({
+    wimdowMs: 15 * 60 * 1000,   // 15 minutes
+    max: 100                    // limit each IP to 100 requests per windowMs
+  })
+)
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);

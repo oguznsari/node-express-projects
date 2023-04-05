92885f3857f4ded5ccc90af92fddb50c4f59b957
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-05 23:44:49 +0300

    testUser middleware no restrict abilities

--- 06.5-jobster-api/middleware/authentication.js
+++ 06.5-jobster-api/middleware/authentication.js
@@ -13,7 +13,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    const testUser = payload.userId === '642c258fc75ab7490c5b8b30';
+    req.user = { userId: payload.userId, testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')

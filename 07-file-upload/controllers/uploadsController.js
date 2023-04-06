455740549533c632caebed6f70b331a4e4234304
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-06 18:23:54 +0300

    products router and controllers + upload

---
+++ 07-file-upload/controllers/uploadsController.js
@@ -0,0 +1,9 @@
+const { StatuseCodes } = require('http-status-codes');
+
+const uploadProductImage = (req, res) => {
+    res.send('upload product image')
+}
+
+module.exports = {
+    uploadProductImage
+}

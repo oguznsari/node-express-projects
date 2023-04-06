cc0ebb5aaf9dad9df80cd8e7d8195767f97bab3a
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-06 19:16:39 +0300

    image upload to the server

--- 07-file-upload/controllers/uploadsController.js
+++ 07-file-upload/controllers/uploadsController.js
@@ -1,7 +1,15 @@
-const { StatuseCodes } = require('http-status-codes');
+const path = require('path');
+const { StatusCodes } = require('http-status-codes');
 
-const uploadProductImage = (req, res) => {
-    res.send('upload product image')
+const uploadProductImage = async (req, res) => {
+    let productImage = req.files.image;
+    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
+
+    await productImage.mv(imagePath);
+
+    res
+        .status(StatusCodes.OK)
+        .json({ image: { src: `/uploads/${productImage.name}` } })
 }
 
 module.exports = {

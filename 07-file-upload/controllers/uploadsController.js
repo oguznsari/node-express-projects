6aa3eb7cd45e0f8e137e575b4f9869721eeb704b
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-06 22:51:51 +0300

    file, mimetype, size checks for image upload

--- 07-file-upload/controllers/uploadsController.js
+++ 07-file-upload/controllers/uploadsController.js
@@ -1,9 +1,27 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors')
 
 const uploadProductImage = async (req, res) => {
-    let productImage = req.files.image;
-    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
+    if (!req.files) {
+        throw new CustomError.BadRequestError('No File uploaded.')
+    }
+    const productImage = req.files.image;
+
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('Please Upload image.')
+    }
+
+    const maxSize = 1024 * 1024;                    // 1MB
+
+    if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('Please upload image smaller 1KB.')
+    }
+
+    const imagePath = path.join(
+        __dirname,
+        '../public/uploads/' + `${productImage.name}`
+    );
 
     await productImage.mv(imagePath);
 

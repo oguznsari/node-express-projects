e312ff8fc9f023b841b76e63fec787da9ba61bb9
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-07 14:45:47 +0300

    sendEmail with Etherial

--- 08-send-email/controllers/sendEmail.js
+++ 08-send-email/controllers/sendEmail.js
@@ -1,5 +1,25 @@
+const nodemailer = require('nodemailer');
+
 const sendEmail = async (req, res) => {
-    res.send('send email func.');
+    let testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: process.env.ETHERIAL_MAIL_USERNAME,
+            pass: process.env.ETHERIAL_MAIL_PASSWORD
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Oğuzhan SARI" <oguzn.sari@gmail.com>',
+        to: 'oguzn.sari@gmail.com',
+        subject: 'Hello',
+        html: '<h2>Sending emails with nodeJs.</h2>'
+    })
+
+    res.json(info);
 }
 
 module.exports = sendEmail;

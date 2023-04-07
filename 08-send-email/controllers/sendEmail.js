58b9342133de9966c6098ef9cf75ef027de4f944
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-07 15:21:35 +0300

    send email with SENDGRID

--- 08-send-email/controllers/sendEmail.js
+++ 08-send-email/controllers/sendEmail.js
@@ -1,14 +1,16 @@
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
-const sendEmail = async (req, res) => {
+// ETHEREAL version
+const sendEmailEthereal = async (req, res) => {
     let testAccount = await nodemailer.createTestAccount();
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: process.env.ETHERIAL_MAIL_USERNAME,
-            pass: process.env.ETHERIAL_MAIL_PASSWORD
+            user: process.env.ETHEREAL_MAIL_USERNAME,
+            pass: process.env.ETHEREAL_MAIL_PASSWORD
         }
     });
 
@@ -20,6 +22,22 @@ const sendEmail = async (req, res) => {
     })
 
     res.json(info);
+}
+
+// SENDGRID option
+const sendEmail = async (req, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+        to: 'oguzn.sari@gmail.com',
+        from: 'emailsend459@gmail.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+    };
+
+    let info = await sgMail.send(msg);
+    res.json(info)
 }
 
 module.exports = sendEmail;

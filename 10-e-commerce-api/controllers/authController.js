70bb76d4ae810600dc41d6e99db23333b30d4283
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-09 18:26:51 +0300

    JWT functions to its own module under utils

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -1,8 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const { createJWT, isTokenValid } = require('../utils')
 
 const register = async (req, res) => {
     const { email, name, password } = req.body;
@@ -18,12 +17,7 @@ const register = async (req, res) => {
     const user = await User.create({ name, email, password, role });
 
     const tokenUser = { name: user.name, userId: user._id, role: user.role }
-    const token = jwt.sign(
-        tokenUser,
-        process.env.JWT_SECRET,
-        {
-            expiresIn: process.env.JWT_LIFETIME
-        });
+    const token = createJWT({ payload: tokenUser })
 
     res.status(StatusCodes.ACCEPTED).json({ user: tokenUser, token })
 }

0b09a6b332fc1e5b1c684e44b0fde64ca1bf53cd
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-09 16:21:06 +0300

    1st user to be admin

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -3,13 +3,17 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors')
 
 const register = async (req, res) => {
-    const { email } = req.body;
+    const { email, name, password } = req.body;
     const emailAlreadyExists = await User.findOne({ email })
     if (emailAlreadyExists) {
         throw new CustomError.BadRequestError('Email already exists.')
     }
 
-    const user = await User.create(req.body);
+    // 1st registered user is an admin
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : 'user';
+
+    const user = await User.create({ name, email, password, role });
     res.status(StatusCodes.ACCEPTED).json(user)
 }
 

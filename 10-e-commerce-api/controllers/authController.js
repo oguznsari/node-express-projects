3a73880fc183dc07f13dd658ce4928403a01d034
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-09 15:49:42 +0300

    unique email double check - both in model and controller

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -1,6 +1,16 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors')
 
 const register = async (req, res) => {
-    res.send('register user')
+    const { email } = req.body;
+    const emailAlreadyExists = await User.findOne({ email })
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError('Email already exists.')
+    }
+
+    const user = await User.create(req.body);
+    res.status(StatusCodes.ACCEPTED).json(user)
 }
 
 const login = async (req, res) => {

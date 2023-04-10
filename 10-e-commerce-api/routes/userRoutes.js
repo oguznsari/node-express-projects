eb8d7406df8821151e7edf206267299bcb36139d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-11 00:23:49 +0300

    authenticateUser & authorizeUser - admin etc.

--- 10-e-commerce-api/routes/userRoutes.js
+++ 10-e-commerce-api/routes/userRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 
 const {
     getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassword
 } = require('../controllers/userController');
 
-router.route('/').get(getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
-router.route('/:id').get(getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;

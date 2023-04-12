7a7793fb4bcb87add89937501a7fbe5c0c21a9c6
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-12 11:21:53 +0300

    showMe -> show current user without querying db (already authenticated)

--- 10-e-commerce-api/controllers/userController.js
+++ 10-e-commerce-api/controllers/userController.js
@@ -17,7 +17,7 @@ const getSingleUser = async (req, res) => {
 }
 
 const showCurrentUser = async (req, res) => {
-    res.send('SHOW current user route.')
+    res.status(StatusCodes.OK).json({ user: req.user })
 }
 
 const updateUser = async (req, res) => {

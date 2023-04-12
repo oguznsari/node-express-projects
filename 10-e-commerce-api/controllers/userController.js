9f853a20fa0409469e921cc3c82d7debf702f69a
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-12 12:29:48 +0300

    updateUser

--- 10-e-commerce-api/controllers/userController.js
+++ 10-e-commerce-api/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { use } = require('../routes/userRoutes');
+const { createTokenUser, attachCookiesToResponse } = require('../utils');
 
 const getAllUsers = async (req, res) => {
     const users = await User.find({ role: 'user' }).select('-password ');
@@ -21,7 +21,20 @@ const showCurrentUser = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
-    res.send('UPDATE user route.');
+    const { email, name } = req.body;
+    if (!email || !name) {
+        throw new CustomError.BadRequestError('Please provide all values.')
+    }
+
+    const user = await User.findOneAndUpdate(
+        { _id: req.user.userId },
+        { email, name },
+        { new: true, runValidators: true }
+    );
+
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+    res.status(StatusCodes.OK).json({ user: tokenUser })
 }
 
 const updateUserPassword = async (req, res) => {

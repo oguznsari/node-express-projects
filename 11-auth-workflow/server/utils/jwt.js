7468942dd5001dcb4c9678f1133fefaf1021bc48
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-18 00:17:16 +0300

    Token Model & accessToken and refreshToken implementation -> createJWT for both

--- 11-auth-workflow/server/utils/jwt.js
+++ 11-auth-workflow/server/utils/jwt.js
@@ -1,15 +1,34 @@
 const jwt = require('jsonwebtoken');
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
-const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
+const isTokenValid = (token) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({ res, user }) => {
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } })
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('accessToken', accessTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+    maxAge: 1000
+  });
+
+  res.cookie('refreshToken', refreshTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+    expires: new Date(Date.now() + oneDay)
+  });
+};
+
+const attachSingleCookieToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
 
   const oneDay = 1000 * 60 * 60 * 24;
@@ -26,4 +45,5 @@ module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
+  attachSingleCookieToResponse
 };

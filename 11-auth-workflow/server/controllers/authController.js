028710601493ec9b127eea349f78aff84ef82976
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-18 10:46:21 +0300

    forgot password token -> 10 minutes on User model

--- 11-auth-workflow/server/controllers/authController.js
+++ 11-auth-workflow/server/controllers/authController.js
@@ -121,9 +121,37 @@ const verifyEmail = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'Email verified.' });
 }
 
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new CustomError.BadRequestError('Please provide valid email.');
+  }
+
+  const user = await User.findOne({ email });
+  if (user) {
+    const passwordToken = crypto.randomBytes(70).toString('hex');
+    // send email.
+
+    const tenMinutes = 1000 * 60 * 10;
+    const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
+
+    user.passwordToken = passwordToken;
+    user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+    await user.save();
+  }
+
+  res.status(StatusCodes.OK).json({ msg: 'Please check your email for reset password link.' });
+}
+
+const resetPassword = async (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: 'reset password.' });
+}
+
 module.exports = {
   register,
   login,
   logout,
-  verifyEmail
+  verifyEmail,
+  forgotPassword,
+  resetPassword
 };

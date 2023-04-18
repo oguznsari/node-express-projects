c696357946da741d87c3bc4a80f46bfea0621448
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-04-18 10:59:29 +0300

    send reset password email

--- 11-auth-workflow/server/controllers/authController.js
+++ 11-auth-workflow/server/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse, createTokenUser, sendVerificationEmail } = require('../utils');
+const { attachCookiesToResponse, createTokenUser, sendVerificationEmail, sendResetPasswordEmail } = require('../utils');
 const crypto = require('crypto');
 
 const register = async (req, res) => {
@@ -131,6 +131,13 @@ const forgotPassword = async (req, res) => {
   if (user) {
     const passwordToken = crypto.randomBytes(70).toString('hex');
     // send email.
+    const origin = 'http://localhost:3000';
+    await sendResetPasswordEmail({
+      name: user.name,
+      email: user.email,
+      token: passwordToken,
+      origin
+    })
 
     const tenMinutes = 1000 * 60 * 10;
     const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);

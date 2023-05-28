6b0bde39a47024ee112fe5e8be29daf95595bcfc
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-28 13:50:00 +0300

    doesn't work lib expired

--- databases/maxcoin/services/backend/MongoBackend.js
+++ databases/maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,49 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+
+const { MongoClient } = require("mongodb");
+
 const CoinAPI = require('../CoinAPI');
 
 class MongoBackend {
 
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = process.env.MONGO_URI;
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    console.log({ url: this.mongoUrl })
+    const mongoClient = new MongoClient(
+      this.mongoUrl,
+      { useUnifiedTopology: true, useNewUrlParser: true });
 
-  async disconnect() {}
+    this.client = mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
+    return this.client;
+  }
 
-  async insert() {}
+  async disconnect() { }
 
-  async getMax() {}
+  async insert() { }
 
-  async max() {}
+  async getMax() { }
+
+  async max() {
+    console.info("Connection to MongoDB");
+    console.time("mongodb-connect");
+    const client = await this.connect();
+    if (client.isConnected()) {
+      console.info("Successfully connected to MongoDB")
+    } else {
+      throw new Error("Connecting to MongoDB failed")
+    }
+
+    console.timeEnd("mongodb-connect");
+  }
 }
 
 module.exports = MongoBackend;

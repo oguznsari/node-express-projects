d22f7e407d4b942e27540961dae0bd6ba01cd70b
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-29 18:34:08 +0300

    order route opened

--- databases/shopper/server/routes/admin/orders/index.js
+++ databases/shopper/server/routes/admin/orders/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const OrderService = require("../../../services/OrderService");
 
-module.exports = () => {
+module.exports = (config) => {
   const router = express.Router();
+  const order = new OrderService(config.mysql.client);
 
-  router.get('/', async (req, res) => {
-    return res.render('admin/orders', {});
-
-    /*
+  router.get('/', async (req, res, next) => {
     try {
       // Get all orders
       const orderResult = await order.getAll();
@@ -21,13 +20,9 @@ module.exports = () => {
       console.error(err);
       return next(err);
     }
-    */
   });
 
-  router.get('/setshipped/:orderId', async (req, res, next) => {
-    return next('Not Implemented');
-
-    /*
+  router.get('/setshipped/:orderId', async (req, res) => {
     try {
       await order.setStatus(req.params.orderId, 'Shipped');
       req.session.messages.push({
@@ -43,7 +38,6 @@ module.exports = () => {
       console.error(err);
       return res.redirect('/admin/orders');
     }
-    */
   });
 
   return router;
